e2808268942a556da0b67116b7421ec22d096362
Chetan Yadav <121376991+Chetan0724@users.noreply.github.com>
2026-02-13 21:11:23 +0530

    Time: 1857 ms (34.61%), Space: 242.9 MB (66.24%) - LeetHub

---
+++ 3714-longest-balanced-substring-ii/3714-longest-balanced-substring-ii.cpp
@@ -0,0 +1,88 @@
+class Solution {
+public:
+    int helper(string& s, int ch1, int ch2) {
+        int n = s.length();
+        unordered_map<int, int> diffMap;
+        int maxL = 0;
+        int count1 = 0;
+        int count2 = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (s[i] != ch1 && s[i] != ch2) {
+                diffMap.clear();
+                count1 = 0;
+                count2 = 0;
+                continue;
+            }
+
+            if (s[i] == ch1)
+                count1++;
+            if (s[i] == ch2)
+                count2++;
+
+            if (count1 == count2) {
+                maxL = max(maxL, count1 + count2);
+            }
+
+            int diff = count1 - count2;
+            if (diffMap.count(diff)) {
+                maxL = max(maxL, i - diffMap[diff]);
+            } else {
+                diffMap[diff] = i;
+            }
+        }
+
+        return maxL;
+    }
+
+    int longestBalanced(string s) {
+        int n = s.length();
+        int maxL = 0;
+
+        int count = 1;
+        for (int i = 1; i < n; i++) {
+            if (s[i] == s[i - 1]) {
+                count++;
+            } else {
+                maxL = max(maxL, count);
+                count = 1;
+            }
+        }
+        maxL = max(maxL, count);
+
+        maxL = max(maxL, helper(s, 'a', 'b'));
+        maxL = max(maxL, helper(s, 'a', 'c'));
+        maxL = max(maxL, helper(s, 'b', 'c'));
+
+        int countA = 0;
+        int countB = 0;
+        int countC = 0;
+        unordered_map<string, int> diffMap;
+
+        for (int i = 0; i < n; i++) {
+            if (s[i] == 'a')
+                countA++;
+            if (s[i] == 'b')
+                countB++;
+            if (s[i] == 'c')
+                countC++;
+
+            if (countA == countB && countA == countC) {
+                maxL = max(maxL, countA + countB + countC);
+            }
+
+            int diffAB = countA - countB;
+            int diffAC = countA - countC;
+
+            string key = to_string(diffAB) + "_" + to_string(diffAC);
+
+            if (diffMap.count(key)) {
+                maxL = max(maxL, i - diffMap[key]);
+            } else {
+                diffMap[key] = i;
+            }
+        }
+
+        return maxL;
+    }
+};
